3a032804873de62d7de170ee4cfe3d9751cdc24c
Adrian Dapprich <adrian.da@outlook.com>
2018-12-24 12:54:18 +0100

    texture now get rendered correctly, I had to use a multi-dim array instead of a single-dim one with size WIDTH*HEIGHT*3. Need to find out why, it seems both have the same underlying memory structure.

--- mandelbrot.h
+++ mandelbrot.h
@@ -6,7 +6,9 @@
 #define MANDELBROT_MANDELBROT_H
 
 #include "properties.h"
+#include <stdio.h>
+#include <math.h>
 
-void mandelbrot_rect(double origin_x, double origin_y, double width, double height, unsigned char *pixels);
+void mandelbrot_rect(double origin_x, double origin_y, double width, double height, unsigned char pixels[SCREEN_HEIGHT][SCREEN_WIDTH][3]);
 
 #endif //MANDELBROT_MANDELBROT_H
